ccf31e259caf4c1df17e97d950760e46e7d5e2aa
Naim Chowdhury <Naim.91@hotmail.com>
2024-01-22 20:49:40 +0100

    derp

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,111 @@
 import { useState } from 'react';
 
-const translations = {
+const translationsSushi = {
   Swedish: {
     1: {
-      dishname: "Blandad Sushi",
-      description: "Blandad Sushi med många olika typer av sushibitar.",
+      dishname: ["Blandad Sushi"],
+      description: ["Blandad Sushi med många olika typer av sushibitar."],
     },
     2: {
-      dishname: "Lax & Rullar",
-      description: "Blandad Sushi som innehåller Rullar och Lax.",
+      dishname: ["Lax & Rullar"],
+      description: ["Blandad Sushi som innehåller Rullar och Lax."],
     },
   },
   English: {
     1: {
-      dishname: "Mixed Sushi",
-      description: "Mixed Sushi with a variety of sushi pieces.",
+      dishname: ["Mixed Sushi"],
+      description: ["Mixed Sushi with a variety of sushi pieces."],
     },
     2: {
-      dishname: "Salmon & Rolls",
-      description: "Mixed Sushi containing Rolls and Salmon.",
+      dishname: ["Salmon & Rolls"],
+      description: ["Mixed Sushi containing Rolls and Salmon."],
     },
   },
   Spanish: {
     1: {
-      dishname: "Sushi Variado",
-      description: "Sushi variado con una variedad de piezas de sushi.",
+      dishname: ["Sushi Variado"],
+      description: ["Sushi variado con una variedad de piezas de sushi."],
     },
     2: {
-      dishname: "Salmón y Rollos",
-      description: "Sushi variado que contiene rollos y salmón.",
+      dishname: ["Salmón y Rollos"],
+      description: ["Sushi variado que contiene rollos y salmón."],
     },
   },
   French: {
     1: {
-      dishname: "Sushi Mixte",
-      description: "Sushi mixte avec une variété de morceaux de sushi.",
+      dishname: ["Sushi Mixte"],
+      description: ["Sushi mixte avec une variété de morceaux de sushi."],
     },
     2: {
-      dishname: "Saumon & Rouleaux",
-      description: "Sushi mixte contenant des rouleaux et du saumon.",
+      dishname: ["Saumon & Rouleaux"],
+      description: ["Sushi mixte contenant des rouleaux et du saumon."],
     },
   },
+};
+
+const translationsNorimaki = {
+  Swedish: {
+    1: {
+      dishname: ["California roll", "California roll Special"],
+      description: ["Inside-Out sjögräsrulle med avocado, gurka, crabbstick och chilimajo", "Inside-Out sjögräsrulle som innehåller avocado, gurka, crabbstick. Toppas med Halstrad lax samt chilimajo"],
+    },
+    2: {
+      dishname: ["Standard roll"],
+      description: ["Inside-Out sjögräsrulle med lax, gurka, crabbstick"],
+    },
+  },
+  English: {
+    1: {
+      dishname: ["California roll", "California roll Special"],
+      description: ["Inside-Out seaweed roll with avocado, cucumber, crabstick, and chili mayo", "Inside-Out seaweed roll containing avocado, cucumber, crabstick. Topped with seared salmon and chili mayo"],
+    },
+    2: {
+      dishname: ["Standard roll"],
+      description: ["Inside-Out seaweed roll with salmon, cucumber, crabstick"],
+    },
+  },
+  Spanish: {
+    1: {
+      dishname: ["California roll", "California roll Special"],
+      description: ["Roll de alga invertida con aguacate, pepino, palitos de cangrejo y mayonesa de chile", "Roll de alga invertida que contiene aguacate, pepino, palitos de cangrejo. Cubierto con salmón a la parrilla y mayonesa de chile"],
+    },
+    2: {
+      dishname: ["Standard roll"],
+      description: ["Roll de alga invertida con salmón, pepino, palitos de cangrejo"],
+    },
+  },
+  French: {
+    1: {
+      dishname: ["California roll", "California roll Special"],
+      description: ["Rouleau d'algue inversé avec avocat, concombre, bâtonnets de crabe et mayonnaise au piment", "Rouleau d'algue inversé contenant de l'avocat, du concombre, des bâtonnets de crabe. Garni de saumon grillé et de mayonnaise au piment"],
+    },
+    2: {
+      dishname: ["Standard roll"],
+      description: ["Rouleau d'algue inversé avec saumon, concombre, bâtonnets de crabe"],
+    },
+  },
+};
+const Menu = ({ name, dishes, translations, getDishDetails }) => {
+  return (
+    <div className="bg-orange-800 text-white">
+      <h2 className="text-2xl font-bold">{name}</h2>
+      {dishes.map((dish) => (
+        <div key={dish.id}>
+          <h3>{dish.dishname.map((name) => name)}</h3>
+          <p>{dish.description.map((desc) => desc)}</p>
+          <div>
+          {dish.pieces.map((piece) => piece)} || {dish.types.map((type) => type)}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 function App() {
   const [selectedLanguage, setSelectedLanguage] = useState('Swedish');
 
-  const getSushiDishDetails = (id) => {
+  const getDishDetails = (translations, id) => {
     const dishTranslation = translations[selectedLanguage][id];
     return {
       dishname: dishTranslation.dishname,
@@ -54,32 +113,10 @@ function App() {
     };
   };
 
-  const Sushi = {
-    language: translations, // Assign the entire translations object to the language property
-    id: "sushi",
-    name: "Sushi Meny",
-    dishes: [
-      {
-        id: 1,
-        pieces: [7, 8, 9, 11, 13, 14, 16, 20],
-        prices: [89, 99, 109, 129, 149, 159, 179, 219],
-        images: [],
-        ...getSushiDishDetails(1),
-      },
-      {
-        id: 2,
-        pieces: [8, 10, 12],
-        prices: [99, 119, 139],
-        images: [],
-        ...getSushiDishDetails(2),
-      },
-    ],
-  };
-
   return (
     <>
-      <h1 className="text-3xl text-center text-white font-bold underline">
-        Hello world!
+      <h1 className="text-6xl text-center text-white font-bold underline">
+        Miyabi
       </h1>
       <select
         name=""
@@ -87,22 +124,56 @@ function App() {
         value={selectedLanguage}
         onChange={(e) => setSelectedLanguage(e.target.value)}
       >
-        {Object.keys(translations).map((language) => (
+        {Object.keys(translationsSushi).map((language) => (
           <option key={language} value={language}>
             {language}
           </option>
         ))}
       </select>
-      <div className="bg-orange-800 text-white">
-        <h2 className="text-2xl font-bold">{Sushi.name}</h2>
-        {Sushi.dishes.map((dish) => (
-          <div key={dish.id}>
-            <h3>{dish.dishname}</h3>
-            <p>{dish.description}</p>
-            {/* Add additional rendering logic for other dish details if needed */}
-          </div>
-        ))}
-      </div>
+
+      <Menu
+        name="Sushi Meny"
+        dishes={[
+          {
+            id: 1,
+            pieces: [7, 8, 9, 11, 13, 14, 16, 20],
+            prices: [89, 99, 109, 129, 149, 159, 179, 219],
+            images: [],
+            ...getDishDetails(translationsSushi, 1),
+          },
+          {
+            id: 2,
+            pieces: [8, 10, 12],
+            prices: [99, 119, 139],
+            images: [],
+            ...getDishDetails(translationsSushi, 2),
+          },
+        ]}
+        translations={translationsSushi}
+        getDishDetails={getDishDetails}
+      />
+
+      <Menu
+        name="Norimaki Meny"
+        dishes={[
+          {
+            id: 1,
+            types: ["Normal", "Special"],
+            prices: [129, 149],
+            images: [],
+            ...getDishDetails(translationsNorimaki, 1),
+          },
+          {
+            id: 2,
+            types: [],
+            prices: [105],
+            images: [],
+            ...getDishDetails(translationsNorimaki, 2),
+          },
+        ]}
+        translations={translationsNorimaki}
+        getDishDetails={getDishDetails}
+      />
     </>
   );
 }
